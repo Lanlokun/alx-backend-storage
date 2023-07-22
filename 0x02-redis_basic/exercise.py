b6e8f73bd075dc75754eed97b66221beb55c554c
malik <mlanlokun@gmail.com>
2023-07-22 03:12:32 +0300

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,46 @@ import redis
 
 from uuid import uuid4
 from typing import Union, Callable, Optional
+from functools import wraps
+
+def count_calls(method: Callable) -> Callable:
+    """counts how many times methods of the Cache class are called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """stores the history of inputs and outputs for a particular
+    function"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+    return wrapper
+
+
+def replay(method: Callable):
+    """displays the history of calls of a particular function"""
+    r = redis.Redis()
+    method_name = method.__qualname__
+    count = r.get(method_name).decode('utf-8')
+    inputs = r.lrange(method_name + ":inputs", 0, -1)
+    outputs = r.lrange(method_name + ":outputs", 0, -1)
+
+    print("{} was called {} times:".format(method_name, count))
+
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(method_name, i.decode('utf-8'),
+                                     o.decode('utf-8')))
 
 
 class Cache:
@@ -40,3 +80,4 @@ class Cache:
     def get_int(self, key: str) -> int:
         """automatically parametrize Cache.get to int"""
         return self.get(key, int)
+    
